6da4b6936821ffbe6b1eb44014580ff36694e73f
FelicityRashetle20 <104845547+FelicityRashetle20@users.noreply.github.com>
2022-06-03 23:44:20 +0200

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,9 @@ int main(void)
 	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 		for (i = 0; i < 26; i++)
-	{
-		putchar(alph[i]);
-	}
+		{
+			putchar(alph[i]);
+		}
 	putchar('\n');
 	return (0);
 }
