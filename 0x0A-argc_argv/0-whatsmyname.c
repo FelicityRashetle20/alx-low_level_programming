f21e9a7d7b233421e3fca428753a2f2879b3f566
Felicity Rashetle <felichrist50@gmail.com>
2023-04-05 06:27:23 -0700

    tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the program name
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
