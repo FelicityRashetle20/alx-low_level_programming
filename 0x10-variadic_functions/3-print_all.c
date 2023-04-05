8bc56b68cd935a51f8700b053007c050b14c198d
Felicity Rashetle <felichrist50@gmail.com>
2023-04-05 14:36:53 -0700

    tasks

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,23 +9,23 @@
 void print_all(const char * const format, ...)
 {
 	va_list valist;
-	unsigned int x = 0, y, c = 0;
+	unsigned int i = 0, j, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
 
 	va_start(valist, format);
 	while (format && format[i])
 	{
-		y = 0;
-		while (t_arg[y])
+		j = 0;
+		while (t_arg[j])
 		{
-			if (format[x] == t_arg[y] && c)
+			if (format[i] == t_arg[j] && c)
 			{
 				printf(", ");
 				break;
-			} y++;
+			} j++;
 		}
-		switch (format[x])
+		switch (format[i])
 		{
 		case 'c':
 			printf("%c", va_arg(valist, int)), c = 1;
@@ -45,7 +45,7 @@ void print_all(const char * const format, ...)
 			}
 			printf("%s", str);
 			break;
-		} x++;
+		} i++;
 	}
 	printf("\n"), va_end(valist);
 }
